31ed71bf24ab1bd477091f43492336d4a2914a04
Guillermo Basulto <guillermobasulto@gmail.com>
2023-11-29 17:05:04 -0600

    Improve documentation.

--- R/create_erd.R
+++ R/create_erd.R
@@ -6,6 +6,16 @@
 #' function takes as its arguments a list of data frames and a list of
 #' relationships and returns a list object of class "ERD".
 #'
+#' \itemize{
+#'  \item{ "||" }{ which indicates one and only one}
+#'  \item{">|" }{ which indicates one or more (left table)}
+#'  \item{"|<" }{ which indicates one or more (right table)}
+#'  \item{">0" }{ which indicates zero or more (left table)}
+#'  \item{"0<" }{ which indicates zero or more (right table)}
+#'  \item{"|0" }{ which indicates zero or one (left table)}
+#'  \item{"0|" }{ which indicates zero or one (right table)}
+#' }
+#'
 #' @param df_list A named list of data frames, where each data frame represents
 #'   an entity in the ERD. The names of the list elements correspond to the
 #'   names of the entities.
