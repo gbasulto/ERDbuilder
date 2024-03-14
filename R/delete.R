6dca4304792f28853f2d968963f5c9091235028b
Guillermo Basulto <guillermobasulto@gmail.com>
2024-03-14 15:16:08 -0500

    Start example.

---
+++ R/delete.R
@@ -0,0 +1,15 @@
+#
+#
+# ## Test
+#
+# ## Define entities
+#
+# students <-
+#   data.frame(
+#     id = c("hu1", "de2", "lo3"),
+#     student = c("Huey", "Dewey", "Louie"),
+#     email = c("hubert.duck", "dewfort.duck", "llewellyn.duck"),
+#     dob = c("04-15", "04-15", "04-15")
+#   )
+#
+# courses <-
