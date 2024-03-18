e18a472fd5d8b1905f4ae3167a493fc0962b8e57
Guillermo Basulto <guillermobasulto@gmail.com>
2024-03-18 17:02:59 -0500

    Update example.

--- R/delete.R
+++ R/delete.R
@@ -4,29 +4,54 @@
 #
 # ## Define entities
 #
-# students <- data.frame(
-#   student_id = c("hu1", "de2", "lo3"),
+# students_tbl <- data.frame(
+#   st_id = c("hu1", "de2", "lo3"),
 #   student = c("Huey", "Dewey", "Louie"),
 #   email = c("hubert.duck", "dewfort.duck", "llewellyn.duck"),
 #   dob = c("04-15", "04-15", "04-15")
 # )
 #
-# courses <- data.frame(
-#   course_id = c("water101", "evil205", "water202"),
+# courses_tbl <- data.frame(
+#   crs_id = c("water101", "evil205", "water202"),
+#   fac_id = c("01pl", "03pe", "02mi"),
 #   course = c("Swimming", "Human-chasing", "Dives")
 # )
 #
-# enrollment <- data.frame(
-#   course_id = c("water101", "evil205", "evil205", "water202"),
-#   student_id = c("hu1", "hu1", "de2", "de2"),
+# enrollment_tbl <- data.frame(
+#   crs_id = c("water101", "evil205", "evil205", "water202"),
+#   st_id = c("hu1", "hu1", "de2", "de2"),
 #   final_grade = c("B", "A", "A", "F")
 # )
 #
-# department <- data.frame(
+# department_tbl <- data.frame(
 #   dept_id = c("water", "evil", "values"),
 #   department = c("Water activities", "Evil procurement", "Good values")
 # )
 #
-# faculty <- data.frame(
-#   faculty_name = c("Pluto", "Mickey", "Pete")
+# faculty_tbl <- data.frame(
+#   faculty_name = c("Scrooge McDuck", "Donald", "Pete", "Mickey"),
+#   fac_id = c("01sc", "02do", "03pe", "04mi"),
+#   dept_id = c("water", "water", "evil", "values")
 # )
+#
+#
+#
+# students_tbl
+# courses_tbl
+# enrollment_tbl
+# department_tbl
+# faculty_tbl
+#
+# erd_object <-
+#   create_erd(
+#
+#     list(
+#       Crash = crash_tbl,
+#       Vehicle = vehicle_tbl,
+#       Occupant = occupant_tbl,
+#       Distract= distract_tbl),
+#     relationships)
+#
+#
+#
+#
