90b1bdb9121d90833612cac141e0ea14403e2398
Guillermo Basulto <guillermobasulto@gmail.com>
2024-03-20 13:03:45 -0500

    Finalize example.

--- R/delete.R
+++ R/delete.R
@@ -6,6 +6,7 @@
 #
 # students_tbl <- data.frame(
 #   st_id = c("hu1", "de2", "lo3"),
+#   dep_id = c("water", "evil", "values"),
 #   student = c("Huey", "Dewey", "Louie"),
 #   email = c("hubert.duck", "dewfort.duck", "llewellyn.duck"),
 #   dob = c("04-15", "04-15", "04-15")
@@ -13,7 +14,7 @@
 #
 # courses_tbl <- data.frame(
 #   crs_id = c("water101", "evil205", "water202"),
-#   fac_id = c("01pl", "03pe", "02mi"),
+#   fac_id = c("02do", "03pe", "04mi"),
 #   dep_id = c("water", "evil", "water"),
 #   course = c("Swimming", "Human-chasing", "Dives")
 # )
@@ -25,17 +26,21 @@
 # )
 #
 # department_tbl <- data.frame(
-#   dept_id = c("water", "evil", "values"),
+#   dep_id = c("water", "evil", "values"),
 #   department = c("Water activities", "Evil procurement", "Good values")
 # )
 #
 # faculty_tbl <- data.frame(
 #   faculty_name = c("Scrooge McDuck", "Donald", "Pete", "Mickey"),
+#   title = c("Emeritus", "Full", "Assistant", "Full"),
 #   fac_id = c("01sc", "02do", "03pe", "04mi"),
-#   dept_id = c("water", "water", "evil", "values")
+#   dep_id = c("water", "water", "evil", "values")
 # )
 #
 #
+#
+#
+# library(ERDbuilder)
 #
 # students_tbl
 # courses_tbl
@@ -45,33 +50,23 @@
 #
 # ## Define relationships
 # relationships <- list(
+#   courses = list(
+#     enrollment = list(crs_id = "crs_id", relationship = c("||", "|<")),
+#     department = list(dep_id = "dep_id", relationship = c(">|", "||")),
+#     faculty = list(fac_id = "fac_id", relationship = c(">0", "||"))
+#   ),
+#   enrollment = list(
+#     students = list(st_id = "st_id", relationship = c(">0", "||")
+#     )
+#   ),
 #   students = list(
-#     enrollment = list(st_id = "st_id", relationship = c("||", "0<"))
+#     department = list(dep_id = "dep_id", relationship = c(">|", "||"))
 #   ),
-#   enrollment = list(),
-#   courses = list(),
-#   department = list(),
-#   faculty = list()
-# )
-#
-# relationships <- list(
-#   Crash = list(
-#     Vehicle = list(
-#       CASENUMBER = "CASENUMBER", relationship = c("||", "|<")),
-#     Occupant = list(
-#       CASENUMBER = "CASENUMBER", relationship = c("||", "|<")),
-#     Distract = list(
-#       CASENUMBER = "CASENUMBER", relationship = c("||", "0<"))
-#   ),
-#   Vehicle = list(
-#     # Crash = list(
-#     #   CASENUMBER = "CASENUMBER", relationship = c("|<", "||")),
-#     Occupant = list(
-#       CASENUMBER = "CASENUMBER", VEHNO = "VEHNO", relationship = c("|0", "0<")),
-#     Distract = list(
-#       CASENUMBER = "CASENUMBER", VEHNO = "VEHNO", relationship = c("||", "0<"))
+#   faculty = list(
+#     department = list(dep_id = "dep_id", relationship = c(">|", "||"))
 #   )
 # )
+#
 #
 # erd_object <-
 #   create_erd(
@@ -80,10 +75,11 @@
 #       courses = courses_tbl,
 #       enrollment = enrollment_tbl,
 #       department = department_tbl,
-#       facutlry = faculty_tbl
+#       faculty = faculty_tbl
 #       ),
 #     relationships)
 #
+# render_erd(erd_object, label_distance = 0, label_angle = 15, n = 20)
 #
-#
+# perform_join(erd_object, c("courses", "enrollment", "department"))
 #
